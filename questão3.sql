6dd21890c30508df762071f31783eef67991102f
gersonSEP <36830145+gersonSEP@users.noreply.github.com>
2020-11-10 17:07:22 -0300

    Update questão3.sql

--- questão3.sql
+++ questão3.sql
@@ -1,9 +1,9 @@
 delimiter $$
 	create procedure tempo_voo(out quantidade int)
 	begin
-	    set @add_milhas := (select milhas
-	    from cliente c, cliente_voo cv
-	    where cliente.c = cliente.cv and classe.cv = executiva);
-	    @add_milhas + 1000;
-	end $$
-	delimiter;
+	set @add_milhas := (select milhas
+	from cliente c, cliente_voo cv
+	where cliente.c = cliente.cv and classe.cv = executiva);
+	@add_milhas + 1000;
+end $$
+delimiter;
